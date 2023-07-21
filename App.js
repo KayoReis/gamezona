4942bf921ec5e4825544eabd6b49b18c2c9b56cb
KayoHReis <kayo.reis@alunos.ifsuldeminas.edu.br>
2023-07-20 21:04:57 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View,Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.containe}>
+      <Text>
+        Oi
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = StyleSheet.create({
+  containe:{
+    paddingVertical:10,
+    paddingHorizontal:20
+  }
+})
