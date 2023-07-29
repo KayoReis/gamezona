d68a0f6b7633b3ed09587318643a12e2c989f3de
KayoHReis <kayo.reis@alunos.ifsuldeminas.edu.br>
2023-07-28 21:11:46 -0300

    icons

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //imports
 import React from 'react';
-import { StyleSheet,StatusBar } from 'react-native';
+import { StyleSheet,StatusBar,Image,View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './src/pages/Home/home_index'
@@ -17,14 +17,37 @@ const Tab=createBottomTabNavigator()
 export default function App(){
   return (
     //login na navegção apenas para teste
-    <NavigationContainer>
+    <NavigationContainer theme={DefaultTheme}  >
       <StatusBar barStyle={'light-content'}/>
-      <Tab.Navigator screenOptions={{headerShown: false}} >
+      <Tab.Navigator screenOptions={{headerShown: false, tabBarShowLabel:false}} >
       <Tab.Screen name='Login' component={Login} />
-      <Tab.Screen name='Home' component={Home}/>
-      <Tab.Screen name='Loja' component={Shop}/>
-      <Tab.Screen name='Comunidade' component={Community}/>
-      <Tab.Screen name='Perfil' component={Perfil}/>
+
+
+
+      <Tab.Screen name='Home' component={Home} options={{
+        tabBarIcon:({focused,size})=>(
+          <View  style={size={size}}>
+          <Image  source={focused? require('./src/img/home_color.png'):require('./src/img/Home_nocolor.png')}/>
+          </View>
+        )}} />
+      <Tab.Screen name='Loja' component={Shop} options={{
+        tabBarIcon:({focused,size})=>(
+          <View style={size=10000} >
+          <Image source={focused? require('./src/img/shop_color.png'):require('./src/img/shop.png')}/>
+          </View>
+        )}}/>
+      <Tab.Screen name='Comunidade' component={Community} options={{
+        tabBarIcon:({focused,size})=>(
+          <View  style={size={size}}>
+          <Image  source={focused? require('./src/img/hand_color.png'):require('./src/img/hand.png')}/>
+          </View>
+        )}}/>
+      <Tab.Screen name='Perfil' component={Perfil} options={{
+        tabBarIcon:({focused,size})=>(
+          <View  style={size={size}}>
+          <Image  source={focused? require('./src/img/perfil_selected.png'):require('./src/img/perfil_unselected.png')}/>
+          </View>
+        )}}/>
     </Tab.Navigator>
     </NavigationContainer>
   );
