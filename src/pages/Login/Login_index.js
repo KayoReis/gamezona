b6eb9d5367e9fad1552bf7e967d6a95e3c608fc9
KayoHReis <kayo.reis@alunos.ifsuldeminas.edu.br>
2023-08-25 01:10:45 -0300

    25/08 1:10

--- src/pages/Login/Login_index.js
+++ src/pages/Login/Login_index.js
@@ -105,6 +105,9 @@ export default function Login({ navigation }) {
           </TouchableOpacity>
         </View>
       </ImageBackground>
+      <Text>
+        teste
+      </Text>
     </View>
   );
 }
